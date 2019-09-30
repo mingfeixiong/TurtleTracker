ba5ff472e0bd168d8b1b321267363e69d504489b
mingfeixiong <mingfei.xiong96@gmail.com>
2019-09-30 12:10:55 -0400

    Adds front matter

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Parses a line of ARGOS tracking data 
+#
+# Created by: John Fay (john.fay@duke.edu)
+# Created on: Fall 2019
+#--------------------------------------------------------------
